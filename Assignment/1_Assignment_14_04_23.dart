0bd24b3596cc8766480815d9865eb133568c4792
Mazharul Islam Tohin <89094831+toh1n@users.noreply.github.com>
2023-04-15 02:16:10 +0600

    Update 1_Assignment_14_04_23.dart

--- Assignment/1_Assignment_14_04_23.dart
+++ Assignment/1_Assignment_14_04_23.dart
@@ -1,3 +1,11 @@
+/*
+Assignment : 1
+Date : 14-04-2023
+Take a list of phone numbers example
+phoneNumber = [ +88, 01768131685, 01768171985, 01768111286, 01768131685]
+And write program that prints out the full number like ‘+8801768171985’. 
+You have to print out all numbers given in the list except +88.
+*/
 main() {
   List<String> phoneNumber = [
     "+88",
