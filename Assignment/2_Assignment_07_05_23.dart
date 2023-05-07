1f82224f2dbdb7f9334a30e4c1174947e7c4e984
Mazharul Islam Tohin <89094831+toh1n@users.noreply.github.com>
2023-05-07 21:34:34 +0600

    Add files via upload

--- Assignment/2_Assignment_07_05_23.dart
+++ Assignment/2_Assignment_07_05_23.dart
@@ -1,3 +1,33 @@
+
+/*
+Name : Mazharul Islam Tohin
+Assignment : 2
+Date : 07-05-2023
+Problem: Student Grading System
+You are building a student grading system. Each student has a name and a test score.
+Your task is to write a Dart function called studentGrade that takes in the student's name and test score,
+and returns their corresponding grade based on the following grading scale:
+
+A: 90 - 100
+B: 80 - 89
+C: 70 - 79
+D: 60 - 69
+F: 0 - 59
+
+The function should validate the test score and ensure it is within the valid range of 0 to 100. 
+If the score is valid, it should return the grade. If the score is invalid, it should return "Invalid Grade".
+Here's an example usage of the studentGrade function:
+void main() {
+  String studentName = "Saiaf Anan";  /// student name must be yours
+  int testScore = 85;
+  String grade = studentGrade(studentName, testScore);
+  print("$studentName's grade: $grade");
+}
+In this example, the studentGrade function should check the test score of your full name and assign the corresponding grade based on the grading scale. 
+Since you scored 85, it falls within the range of 80-89, so the function should return "B".
+*/
+
+
 main() {
   String studentName = " Mazharul Islam Tohin";
 
